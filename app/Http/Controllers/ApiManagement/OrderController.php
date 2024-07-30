b293923d5f5fcfbe02e3f5c6353d658327352f00
Nitesh Shah <niteshshah000@gmail.com>
2024-07-30 17:30:53 +0530

    add order list function

--- app/Http/Controllers/ApiManagement/OrderController.php
+++ app/Http/Controllers/ApiManagement/OrderController.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Validator;
 
 use App\Http\Controllers\Controller;
 
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
@@ -18,7 +20,8 @@ use App\Models\Address;
 use App\Models\Order;
 
 use App\Models\Cart;
-use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
 
 class OrderController extends Controller
 {
@@ -154,9 +157,18 @@ class OrderController extends Controller
                 'type'              => $typeData
             ];
 
-            OrderDetail::created([
-
+          $orderdetail =  OrderDetail::create([          
+                'product_id'            =>  $product->id,
+                'type_id'               =>  $cartItem->type_id,
+                'type_mrp'              =>  $cartItem->type->mrp,
+                'gst'                   =>  $cartItem->type->gst_percentage,
+                'gst_percentage_price'  =>  $cartItem->type->gst_percentage_price,
+                'quantity'              =>  $cartItem->quantity,
+                'amount'                =>  $cartItem->total_qty_price,
+                'ip'                    =>  $request->ip(),
+                'date'                  =>  now()->setTimezone('Asia/Kolkata')->format('Y-m-d H:i:s')
             ]);
+
         }
 
         // Fetch user address
@@ -203,17 +215,17 @@ class OrderController extends Controller
 
                 $gift_card_gst_amount = $applyGiftCard['gst_amount'];
 
-                $gift_card_1     = $applyGiftCard['card_1'];
+                $gift_card_1     = $applyGiftCard['card_1']['id'];
 
                 $gift_card_2_id     = $applyGiftCard['s_id'];
             }
         }
-
-       $store  =  Order::created([
-            'user_id'                    => Auth::user()->id,
+       
+        $order =  Order::create([
+            'user_id'                    => $userId ?? Auth::user()->id,
             'total_amount'               => $totalAmount,
             'address_id'                 => $addressId,
-            'promocode'                  => $promocodeId,
+            'promocode'                  => $promocodeId ?? '',
             'promo_deduction_amount'     => $deductionAmount,
             'gift_id'                    => $gift_card_1,
             'gift_amt'                   => $gift_card_amount,
@@ -233,8 +245,15 @@ class OrderController extends Controller
             'payment_type'               => 0,
 			'payment_status'             => 0,
             'ip'                         => $request->ip(),
-			'date'                       => now()->setTimezone('Asia/Kolkata')->format('Y-m-d')
+			'date'                       => now()->setTimezone('Asia/Kolkata')->format('Y-m-d H:i:s')
        ]);
+
+       if($order){
+
+          OrderDetail::where('id' ,$orderdetail->id)->update(['main_id' => $order->id]);
+
+       }
+
         // Return the calculated data (or proceed with further processing)
         return response()->json([
             'success'         => true,
@@ -247,4 +266,89 @@ class OrderController extends Controller
             'promo_discount'  => $deductionAmount,
         ]);
     }
+
+    public function orders(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id'      => 'required|exists:users,id',
+            'device_id'    => 'required',
+            'lang'         => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+
+            return response()->json(['message' => $validator->errors()->first(), 'status' => 400]);
+        }
+
+        $user_id = $request->input('user_id');
+
+        $lang    = $request->input('lang');
+
+        $user = User::find($user_id);
+
+        $dataw = [];
+
+        if ($user) {
+
+            $orders = Order::where('user_id', $user->id)->orderBy('id', 'DESC')->get();
+
+            if ($orders->isNotEmpty()) {
+
+                foreach ($orders as $order) {
+
+                    $promo = '';
+
+                    if ($order->promocode != 'Apply coupon' && !empty($order->promocode)) {
+
+                        $promocode = Promocode::find($order->promocode);
+
+                        if ($promocode) {
+
+                            $promo = $promocode->promocode;
+
+                        }
+
+                    }
+
+                    $payment_type = '';
+
+                    if ($order->payment_type == 1) {
+
+                        $payment_type = $lang != 'hi' ? 'Cash on delivery' : lang_change('Cash on delivery');
+
+                    } elseif ($order->payment_type == 2) {
+
+                        $payment_type = $lang != 'hi' ? 'Online Payment' : lang_change('Online Payment');
+
+                    }
+
+                    $dataw[] = [
+                        'order_id'     => $order->id,
+                        'order_status' => $order->order_status,
+                        'sub_total'    => $order->sub_total,
+                        'amount'       => $order->total_amount,
+                        'promocode_discount' => $order->promo_deduction_amount,
+                        'delivery_charge' => $order->delivery_charge,
+                        'cod_charge'      => $order->cod_charge,
+                        'payment_type'    => $payment_type,
+                        'date'            => $order->date,
+                        'promocode'       => $promo
+                    ];
+                }
+            }
+
+            return response()->json([
+                'message' => 'success',
+                'data' => $dataw,
+                'status' => 200
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'success',
+            'data' => $dataw,
+            'status' => 200
+        ]);
+    }
+
 }
