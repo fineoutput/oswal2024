6805ab07d9b512640cad385e3957709396ee5407
Nitesh Shah <niteshshah000@gmail.com>
2024-08-24 18:11:04 +0530

    remove status

--- app/Http/Controllers/ApiManagement/DeliveryBoyController.php
+++ app/Http/Controllers/ApiManagement/DeliveryBoyController.php
@@ -389,7 +389,7 @@ class DeliveryBoyController extends Controller
             ]);
         }
 
-        $order->status = 2;
+        $order->status = 1;
         $order->accepted_at = now()->setTimezone('Asia/Kolkata')->format('Y-m-d H:i:s');
         $order->save();
 
