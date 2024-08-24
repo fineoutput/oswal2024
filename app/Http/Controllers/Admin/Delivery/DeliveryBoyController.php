44a4b7d0b43816de3b363ee8d3ea16eeed7ad9ff
Nitesh Shah <niteshshah000@gmail.com>
2024-08-24 15:17:09 +0530

    add trim i n password fild

--- app/Http/Controllers/Admin/Delivery/DeliveryBoyController.php
+++ app/Http/Controllers/Admin/Delivery/DeliveryBoyController.php
@@ -75,7 +75,7 @@ class DeliveryBoyController extends Controller
 
         if ($request->password) {
 
-            $user->password = Hash::make($request->password);
+            $user->password = Hash::make(trim($request->password));
 
         }
 
