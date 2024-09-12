97915055cfed05c622a6919bb3e35607bc9c47b8
Rohit pancholi <rohitpancholi14510@gmail.com>
2024-09-12 15:23:53 +0530

    chngs

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -109,7 +109,7 @@ display:none;
     <path d="M17.5 17.5L22 22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
     <path d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
 </svg>
-                        </button> -->
+                        </button>
                         <input type="search" class="search-field d-none d-lg-block" placeholder="Search …" value="" name="s" title="Search for:" />
                         @php
                             $count = 0;
