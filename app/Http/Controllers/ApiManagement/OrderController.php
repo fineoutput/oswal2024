57864165d00373487baa1f2944da7e91363d6d54
Nitesh Shah <niteshshah000@gmail.com>
2024-09-14 16:32:29 +0530

    add

--- app/Http/Controllers/ApiManagement/OrderController.php
+++ app/Http/Controllers/ApiManagement/OrderController.php
@@ -795,7 +795,7 @@ class OrderController extends Controller
     public function orders(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'user_id'      => 'required|exists:users,device_id',
+            'user_id'      => 'required|exists:users,id',
             'device_id'    => 'required',
             'lang'         => 'required|string'
         ]);
