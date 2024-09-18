c96bde6191d3745e77ebf608f8f46593243b59cc
Nitesh Shah <niteshshah000@gmail.com>
2024-09-18 17:49:28 +0530

    add

--- routes/admin.php
+++ routes/admin.php
@@ -605,8 +605,6 @@ Route::prefix('user')->name('user.')->group(function () {
 
     Route::get('index', [UsersController::class, 'index'])->name('index');
 
-    Route::get('vendor', [UsersController::class, 'index'])->name('vendor');
-
     Route::get('create/{id?}', [UsersController::class, 'create'])->name('create');
 
     Route::post('store', [UsersController::class, 'store'])->name('store');
