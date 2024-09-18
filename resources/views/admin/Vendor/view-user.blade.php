a0f2dfe3be64798b5ab775fb26c11427f9a0303c
Nitesh Shah <niteshshah000@gmail.com>
2024-09-18 18:08:33 +0530

    add

--- resources/views/admin/Vendor/view-user.blade.php
+++ resources/views/admin/Vendor/view-user.blade.php
@@ -127,6 +127,7 @@
                                                 @foreach ($users as $key => $user)
                                                 @php
                                                    $vendor =  $user->vendor;
+                                                   dd($vendor);
                                                 @endphp
                                                 <tr>
                                                     <td>{{ ++$key }}</td>
