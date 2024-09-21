8a40bf11f39a30237626bf911e5ba023bc08840d
Nitesh Shah <niteshshah000@gmail.com>
2024-09-21 18:28:14 +0530

    add

--- resources/views/partials/homeparts/testimonials.blade.php
+++ resources/views/partials/homeparts/testimonials.blade.php
@@ -1,3 +1,10 @@
+@php
+   $testimonials = App\Models\Testimonial::where('is_active', 1)->orderBy('id', 'desc')->limit(10)->get();
+
+@endphp
+
+@if (count($testimonials)>0)
+    
 <section class="testtimonial_sect section_padding">
 
     <div class="container">
@@ -24,7 +31,56 @@
 
                     <ul class="splide__list">
 
+                        @forelse ($testimonials as $value)
+                            
                         <li class="splide__slide">
+
+                            <div class="card set_card">
+
+                                <div class="img">
+
+                                    <img src="{{ asset($value->image) }}" alt="img" draggable="false" />
+
+                                </div>
+
+                                <h2>Blanche Pearson</h2>
+
+                                <span class="text-center">{{ $value->description }}</span>
+
+                                <div class="rating">
+
+                                    @php
+                                        $ratings = [
+                                            5 => 'Awesome',
+                                            4 => 'Great',
+                                            3 => 'Very good',
+                                            2 => 'Good',
+                                            1 => 'Bad'
+                                        ];
+
+                                        $currentRating = $value->rating;
+
+                                    @endphp
+
+                                    @foreach ($ratings as $value => $title)
+
+                                    <input type="radio" id="star{{ $value }}" name="rating" value="{{ $value }}" {{ $value <= $currentRating ? 'checked' : '' }} />
+
+                                    <label class="star" for="star{{ $value }}" title="{{ $title }}" aria-hidden="true"></label>
+
+                                    @endforeach
+                                    
+                                </div>
+
+                            </div>
+
+                        </li>
+
+                        @empty
+                            Not Found 
+                        @endforelse
+
+                        {{-- <li class="splide__slide">
 
                             <div class="card set_card">
 
@@ -106,49 +162,7 @@
 
                             </div>
 
-                        </li>
-
-                        <li class="splide__slide">
-
-                            <div class="card set_card">
-
-                                <div class="img">
-
-                                    <img src="https://t4.ftcdn.net/jpg/03/50/40/93/240_F_350409330_2bqhjowfBmrqEia5U8lBsGrvD7h8EIo6.jpg" alt="img" draggable="false" />
-
-                                </div>
-
-                                <h2>Blanche Pearson</h2>
-
-                                <span class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa enim tenetur exercitationem.</span>
-
-                                <div class="rating">
-
-                                    <input type="radio" id="star5" name="rating" value="5" />
-
-                                    <label class="star" for="star5" title="Awesome" aria-hidden="true"></label>
-
-                                    <input type="radio" id="star4" name="rating" value="4" />
-
-                                    <label class="star" for="star4" title="Great" aria-hidden="true"></label>
-
-                                    <input type="radio" id="star3" name="rating" value="3" />
-
-                                    <label class="star" for="star3" title="Very good" aria-hidden="true"></label>
-
-                                    <input type="radio" id="star2" name="rating" value="2" />
-
-                                    <label class="star" for="star2" title="Good" aria-hidden="true"></label>
-
-                                    <input type="radio" id="star1" name="rating" value="1" />
-
-                                    <label class="star" for="star1" title="Bad" aria-hidden="true"></label>
-
-                                </div>
-
-                            </div>
-
-                        </li>
+                        </li> --}}
 
                         <!-- Repeat for other slides -->
                         <!-- Make sure to use the same format for other items -->
@@ -164,14 +178,5 @@
 
 </section>
 
-<!-- ///////////////Banner Section 2 Starts///////////////// -->
+@endif
 
-{{-- <div class="post_banner py-10" style="background-image: url('{{ asset('images/banner_sect.jpeg') }}');">
-
-    <div class="container">
-
-        <div class="row"></div>
-
-    </div>
-
-</div> --}}
