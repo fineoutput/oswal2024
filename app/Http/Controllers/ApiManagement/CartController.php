6cad442358ff328aeae859edd87893b9e80201cf
Nitesh Shah <niteshshah000@gmail.com>
2024-09-25 17:56:28 +0530

    add

--- app/Http/Controllers/ApiManagement/CartController.php
+++ app/Http/Controllers/ApiManagement/CartController.php
@@ -134,16 +134,16 @@ class CartController extends Controller
         $data['updated_at'] = $curDate;
 
         // Handle backup in CartOld
-        $backupCartItem = CartOld::where('device_id', $data['device_id'])
+        $backupCartItem = CartOld::where('product_id', $data['product_id'])
 
-            ->where('product_id', $data['product_id'])
+                                 ->where('device_id', $data['device_id']);
 
-            ->when(!empty($data['user_id']), function ($query) use ($data) {
-
-                $query->orwhere('user_id', $data['user_id']);
-            })
-            ->first();
-
+                                 if (!empty($request->user_id)) {
+                                    $backupCartItem->where('user_id', $request->user_id);
+                                 } 
+            
+                                $backupCartItem= $$backupCartItem->first();
+            
 
         if (empty($backupCartItem)) {
 
@@ -162,7 +162,8 @@ class CartController extends Controller
 
         // Handle current cart in Cart
         $cartItem = Cart::where('product_id', $data['product_id'])
-                     ->where('device_id', $data['device_id']);
+
+                    ->where('device_id', $data['device_id']);
 
                     if (!empty($request->user_id)) {
                         $cartItem->where('user_id', $request->user_id);
