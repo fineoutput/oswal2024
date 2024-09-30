f048e1b234349074e34ff52672d516ad3af153ed
Nitesh Shah <niteshshah000@gmail.com>
2024-09-30 17:05:04 +0530

    add cart

--- app/Http/Controllers/ApiManagement/CartController.php
+++ app/Http/Controllers/ApiManagement/CartController.php
@@ -135,15 +135,14 @@ class CartController extends Controller
 
         // Handle backup in CartOld
         $backupCartItem = CartOld::where('product_id', $data['product_id'])
+                         ->where(function($query) use ($data, $request) {
+                             $query->where('device_id', $data['device_id']);
 
-                                 ->where('device_id', $data['device_id']);
-
-                                 if (!empty($request->user_id)) {
-                                    $backupCartItem->where('user_id', $request->user_id);
-                                 } 
-            
-                                $backupCartItem= $backupCartItem->first();
-            
+                             if (!empty($request->user_id)) {
+                                 $query->orWhere('user_id', $request->user_id);
+                             }
+                         })
+                         ->first();
 
         if (empty($backupCartItem)) {
 
