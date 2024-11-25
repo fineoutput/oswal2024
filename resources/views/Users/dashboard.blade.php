c5bfa799fd4ef59ec90dbe4ec3552a6834169fc5
Ayush <ayushrs7023@gmail.com>
2024-11-25 12:40:55 +0530

    rating

--- resources/views/Users/dashboard.blade.php
+++ resources/views/Users/dashboard.blade.php
@@ -5,32 +5,32 @@
 @section('content')
 <style>
 .star-rating {
-  border:solid 1px #ccc;
-  display:flex;
+  border: solid 1px #ccc;
+  display: flex;
   flex-direction: row-reverse;
-  font-size:1.5em;
-  justify-content:space-around;
-  padding:0 .2em;
-  text-align:center;
-  width:5em;
+  font-size: 1.5em;
+  justify-content: space-around;
+  padding: 0 .2em;
+  text-align: center;
+  width: 5em;
 }
 
 .star-rating input {
-  display:none;
+  display: none;
 }
 
 .star-rating label {
-  color:#ccc;
-  cursor:pointer;
+  color: #ccc;
+  cursor: pointer;
 }
 
 .star-rating :checked ~ label {
-  color:#f90;
+  color: #f90;
 }
 
 .star-rating label:hover,
 .star-rating label:hover ~ label {
-  color:#fc0;
+  color: #fc0;
 }
 </style>
 <div class="container-fluid">
@@ -115,20 +115,25 @@
         </a>
     @endif
 </td>
-                        <td>
-                        <div class="star-rating">
-  <input type="radio" id="5-stars" name="rating" value="5" />
-  <label for="5-stars" class="star">&#9733;</label>
-  <input type="radio" id="4-stars" name="rating" value="4" />
-  <label for="4-stars" class="star">&#9733;</label>
-  <input type="radio" id="3-stars" name="rating" value="3" />
-  <label for="3-stars" class="star">&#9733;</label>
-  <input type="radio" id="2-stars" name="rating" value="2" />
-  <label for="2-stars" class="star">&#9733;</label>
-  <input type="radio" id="1-star" name="rating" value="1" />
-  <label for="1-star" class="star">&#9733;</label>
-</div>
-                        </td>
+<td>
+    <div class="star-rating">
+        <!-- Dynamically create unique IDs for each rating -->
+        <input type="radio" id="5-stars-order{{ $order['order_id'] }}" name="rating-order{{ $order['order_id'] }}" value="5" />
+        <label for="5-stars-order{{ $order['order_id'] }}" class="star">&#9733;</label>
+
+        <input type="radio" id="4-stars-order{{ $order['order_id'] }}" name="rating-order{{ $order['order_id'] }}" value="4" />
+        <label for="4-stars-order{{ $order['order_id'] }}" class="star">&#9733;</label>
+
+        <input type="radio" id="3-stars-order{{ $order['order_id'] }}" name="rating-order{{ $order['order_id'] }}" value="3" />
+        <label for="3-stars-order{{ $order['order_id'] }}" class="star">&#9733;</label>
+
+        <input type="radio" id="2-stars-order{{ $order['order_id'] }}" name="rating-order{{ $order['order_id'] }}" value="2" />
+        <label for="2-stars-order{{ $order['order_id'] }}" class="star">&#9733;</label>
+
+        <input type="radio" id="1-star-order{{ $order['order_id'] }}" name="rating-order{{ $order['order_id'] }}" value="1" />
+        <label for="1-star-order{{ $order['order_id'] }}" class="star">&#9733;</label>
+    </div>
+</td>
 
                             </tr>
                             @empty
