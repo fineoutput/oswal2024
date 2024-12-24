1e0bf2d3e32b087415c4016fbe85ee447c866757
Nitesh Shah <niteshshah000@gmail.com>
2024-12-24 16:57:14 +0530

    //

--- app/Http/Controllers/ApiManagement/OrderController.php
+++ app/Http/Controllers/ApiManagement/OrderController.php
@@ -1058,7 +1058,7 @@ class OrderController extends Controller
                     
                     $rating_avg = number_format((float)$rating_avg, 1, '.', '');
                     $tracktransfer = TransferOrder::where('order_id',$order->id)->get();
-                    return $tracktransfer;
+                    return $tracktransfer->status;
                     $dataw[] = [
                         'order_id'        => $order->id,
                         'order_status'    => getOrderStatus($order->order_status),
