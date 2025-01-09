aa54328e98a9c7f3e9a5954a5d122c4809b2b4ad
Nitesh Shah <niteshshah000@gmail.com>
2025-01-09 18:17:49 +0530

    //

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -226,7 +226,7 @@ foreach ($orders as $order) {
 
         }
 
-        if ($order_status == 2) {
+        if ($order_status == 3) {
             $dilivery_order = TransferOrder::where('order_id',$order->id)->first();
             return $dilivery_order;
             $dilivery_boy = DeliveryBoy::where('id',$dilivery_order->user_id)->first();
