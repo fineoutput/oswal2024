95cd2e444a6cd347a2cf5276ceab6a62c0923c79
Ayush <ayushrs7023@gmail.com>
2025-01-11 15:54:35 +0530

    submit

--- resources/views/partials/homeparts/about.blade.php
+++ resources/views/partials/homeparts/about.blade.php
@@ -137,21 +137,20 @@
 }
  }
     </style>
-@if($latestPopupImage->web_image)
+@if(isset($latestPopupImage) && $latestPopupImage->web_image)
 <div id="myModal" class="modal">
     <div class="modal-content">
         <div class="modal-left">
             {{-- <h4 style="text-align: center;">Oswal Products</h4> --}}
         </div>
         <div class="modal-right">
-            
             <img src="{{ asset($latestPopupImage->web_image) }}" alt="Popup Image" style="width:100%;">
-            
         </div>
         <span class="close">&times;</span>
     </div>
 </div>
 @endif
+
 <section class="module"
 
     style="background-image: url('{{ asset('images/3.webp') }}'); margin-top: -160px; z-index: 9; background-size: cover; background-position: 30% 50%; text-align: center;">
