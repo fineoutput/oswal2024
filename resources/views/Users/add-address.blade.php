32c828095670cd898e082b72723bea19dd800d71
Ayush <ayushrs7023@gmail.com>
2025-01-17 12:34:14 +0530

    address

--- resources/views/Users/add-address.blade.php
+++ resources/views/Users/add-address.blade.php
@@ -44,137 +44,144 @@
     </style>
 
     <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+            <h2>Add Address</h2>
 
-        <h2>Add Address</h2>
+<form action="{{ route('user.stor-address') }}" method="post">
 
-        <form action="{{ route('user.stor-address') }}" method="post">
+    @csrf
 
-            @csrf
+    @if ($address != null)
+     <input type="hidden" name="address_id" value="{{$address->id}}">
+      @endif
 
+    <input type="hidden" value="{{ $redirect }}" name="redirect">
+    
+    <div class="form-group">
+
+        <input type="hidden" name="name" value="{{ Auth::user()->first_name }}" required>
+        
+    </div>
+
+    <div class="form-group">
+
+        <label for="lastName">Doorflat</label>
+
+        <input type="text" id="doorflat" name="doorflat" value="{{ $address ? $address->doorflat : old('doorflat') }}" required>
+
+        @error('doorflat')
+
+            <div style="color:red">{{ $message }}</div>
+
+        @enderror
+    </div>
+
+    <div class="form-group">
+
+        <label for="email">Landmark</label>
+
+        <input type="text" id="landmark" name="landmark" value="{{ $address ? $address->landmark : old('landmark') }}" required>
+
+        @error('landmark')
+
+            <div style="color:red">{{ $message }}</div>
+
+        @enderror
+    </div>
+
+    <div class="form-group">
+
+        <label for="state">State</label>
+
+        <select class="form-control select2 p-0 pt-2" name="state" id="addressstate" onchange="getCity('{{ route('getcity') }}', 'city-container1')">
+            <option value="99999">Choose State</option>
+            @foreach ($states as $state)
+                <option value="{{ $state->id }}" {{ old('state') == $state->id || (isset($address) && $address->state == $state->id) ? 'selected' : '' }}>
+                    {{ $state->state_name }}
+                </option>
+            @endforeach
+        </select>
+
+        @error('state')
+
+            <div style="color:red">{{ $message }}</div>
+
+        @enderror
+    </div>
+
+    <div class="form-group">
+
+        <label for="city">City</label>
+
+        <select class="form-control select2 p-0 pt-2" name="city" id="city-container1">
+            <option value="">----- Select City -----</option>
             @if ($address != null)
-             <input type="hidden" name="address_id" value="{{$address->id}}">
-              @endif
+                @foreach ($cities as $citie)
+                    <option value="{{ $citie->id }}" {{ isset($address) && $address->city == $citie->id ? 'selected' : '' }}>
+                        {{ $citie->city_name }}
+                    </option>
+                @endforeach
+            @endif
+        </select>
 
-            <input type="hidden" value="{{ $redirect }}" name="redirect">
-            
-            <div class="form-group">
+        @error('city')
 
-                <input type="hidden" name="name" value="{{ Auth::user()->first_name }}" required>
-                
+            <div style="color:red">{{ $message }}</div>
+
+        @enderror
+
+    </div>
+
+    <div class="form-group">
+
+        <label for="address">Address</label>
+
+        <input type="text" id="address" name="address" value="{{ $address ? $address->address : old('address') }}" required>
+
+        @error('address')
+
+            <div style="color:red">{{ $message }}</div>
+
+        @enderror
+    </div>
+
+    <div class="form-group">
+
+        <label for="zipcode">Zipcode</label>
+
+        <input type="text" id="zipcode" name="zipcode" value="{{ $address ? $address->zipcode : old('zipcode') }}" required>
+
+        @error('zipcode')
+
+            <div style="color:red">{{ $message }}</div>
+
+        @enderror
+    </div>
+    <input type="hidden" name="latitude" id="latitudeInput">
+    <input type="hidden" name="longitude" id="longitudeInput">
+
+    <div class="form-group">
+
+        <button type="">Update Information</button>
+
+    </div>
+
+</form>
             </div>
-
-            <div class="form-group">
-
-                <label for="lastName">Doorflat</label>
-
-                <input type="text" id="doorflat" name="doorflat" value="{{ $address ? $address->doorflat : old('doorflat') }}" required>
-
-                @error('doorflat')
-
-                    <div style="color:red">{{ $message }}</div>
-
-                @enderror
-            </div>
-
-            <div class="form-group">
-
-                <label for="email">Landmark</label>
-
-                <input type="text" id="landmark" name="landmark" value="{{ $address ? $address->landmark : old('landmark') }}" required>
-
-                @error('landmark')
-
-                    <div style="color:red">{{ $message }}</div>
-
-                @enderror
-            </div>
-
-            <div class="form-group">
-
-                <label for="state">State</label>
-
-                <select class="form-control select2 p-0 pt-2" name="state" id="addressstate" onchange="getCity('{{ route('getcity') }}', 'city-container1')">
-                    <option value="99999">Choose State</option>
-                    @foreach ($states as $state)
-                        <option value="{{ $state->id }}" {{ old('state') == $state->id || (isset($address) && $address->state == $state->id) ? 'selected' : '' }}>
-                            {{ $state->state_name }}
-                        </option>
-                    @endforeach
-                </select>
-
-                @error('state')
-
-                    <div style="color:red">{{ $message }}</div>
-
-                @enderror
-            </div>
-
-            <div class="form-group">
-
-                <label for="city">City</label>
-
-                <select class="form-control select2 p-0 pt-2" name="city" id="city-container1">
-                    <option value="">----- Select City -----</option>
-                    @if ($address != null)
-                        @foreach ($cities as $citie)
-                            <option value="{{ $citie->id }}" {{ isset($address) && $address->city == $citie->id ? 'selected' : '' }}>
-                                {{ $citie->city_name }}
-                            </option>
-                        @endforeach
-                    @endif
-                </select>
-
-                @error('city')
-
-                    <div style="color:red">{{ $message }}</div>
-
-                @enderror
-
-            </div>
-
-            <div class="form-group">
-
-                <label for="address">Address</label>
-
-                <input type="text" id="address" name="address" value="{{ $address ? $address->address : old('address') }}" required>
-
-                @error('address')
-
-                    <div style="color:red">{{ $message }}</div>
-
-                @enderror
-            </div>
-
-            <div class="form-group">
-
-                <label for="zipcode">Zipcode</label>
-
-                <input type="text" id="zipcode" name="zipcode" value="{{ $address ? $address->zipcode : old('zipcode') }}" required>
-
-                @error('zipcode')
-
-                    <div style="color:red">{{ $message }}</div>
-
-                @enderror
-            </div>
-            <input type="hidden" name="latitude" id="latitudeInput">
-            <input type="hidden" name="longitude" id="longitudeInput">
-
-            <div class="form-group">
-
-                <button type="">Update Information</button>
-
-            </div>
-
-        </form>
-
-        <h1>Google Maps Location Picker</h1>
+            <div class="col-lg-6">
+            <h1>Google Maps Location Picker</h1>
 <div id="map" style="height: 500px;"></div>
 <p>Selected Location: <span id="location"></span></p>
 <button class="animated-button" id="locateButton">Get current location</button>
 </div>
 <!-- Load Google Maps API -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZ5Ns4p7EPERf63-neMQvYI8EqYnW3Vns&callback=initMap&libraries=places" async defer></script>
+
+            </div>
+        </div>
+        
+
 
   <!-- Main Script -->
   <script>
