7d60aa3abcb4f11f5197907ee02b92709654a85e
Nitesh Shah <niteshshah000@gmail.com>
2025-01-24 11:02:07 +0530

    //

--- app/Http/Controllers/Frontend/CheckOutController.php
+++ app/Http/Controllers/Frontend/CheckOutController.php
@@ -87,7 +87,9 @@ class CheckOutController extends Controller
 
             $userAddressid = Address::findOrFail($addressId);
 
-return $orderdetails;
+            $product_id = $orderdetails['order_details'][0]['product_id'];
+            $type_name = $orderdetails['order_details'][0]['type']['type_name'];
+return $product_id;
 
             $updateprice = Type::where('product_id',$orderdetails->product_id)->where('state_id',$userAddressid->state)->where('city_id',$userAddressid->city)->get(); 
             // return $updateprice;
