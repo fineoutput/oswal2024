bb06b62a49766f3526670b99da43144ad5fb2dfd
Nitesh Shah <niteshshah000@gmail.com>
2025-03-27 14:35:04 +0530

    //

--- app/Http/Controllers/ApiManagement/OrderController.php
+++ app/Http/Controllers/ApiManagement/OrderController.php
@@ -1263,7 +1263,7 @@ class OrderController extends Controller
                     if ($tracktransfer && $tracktransfer->deliveryBoy && $tracktransfer->deliveryBoy->role_type == 2) {
                         // If the deliveryBoy's role type is 2 and there is an image, assign the invoice URL
                         if ($tracktransfer->image) {
-                            $invoice_url = asset('uploads/image/' . $tracktransfer->image);
+                            $invoice_url = asset($tracktransfer->image);
                         } else {
                             // If no image exists, you can set a default message or handle accordingly
                             $invoice_url = null; // or you can set a default image URL
