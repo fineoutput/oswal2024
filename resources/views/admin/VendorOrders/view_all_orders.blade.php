d7b5cf389dbea9c7e5ac43e3fcbf905724815974
Nitesh Shah <niteshshah000@gmail.com>
2025-03-28 18:37:23 +0530

    //

--- resources/views/admin/VendorOrders/view_all_orders.blade.php
+++ resources/views/admin/VendorOrders/view_all_orders.blade.php
@@ -344,18 +344,15 @@
                                                     
                                                                     @if ($order->order_status == 1)
                                                                     @if($order->user->role_type == 2)
-                                                                        <li>
-                                                                            <a href="{{ route('order.vendor.update-status', ['id' => base64_encode($order->id), 'status' => base64_encode(2)]) }}">
-                                                                                Accept Confirm
-                                                                            </a>
-                                                                        </li>
-                                                                        @else
+                                                                            <script>
+                                                                                window.location.href = "{{ route('order.vendor.update-status', ['id' => base64_encode($order->id), 'status' => base64_encode(2)]) }}";
+                                                                            </script>
+                                                                        @endif
                                                                         <li>
                                                                             <a href="{{ route('order.vendor.update-status', ['id' => base64_encode($order->id), 'status' => base64_encode(2)]) }}">
                                                                                 Accept Order Confirm
                                                                             </a>
                                                                         </li>
-                                                                        @endif
                                                                         <li>
                                                                             <a href="{{ route('order.vendor.update-status', ['id' => base64_encode($order->id), 'status' => base64_encode(5)]) }}">
                                                                                 Reject
