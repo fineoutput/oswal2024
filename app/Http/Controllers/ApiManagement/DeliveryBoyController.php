cba85a39055066bb5f9e7ba8717423540d65db95
Nitesh Shah <niteshshah000@gmail.com>
2025-04-01 18:52:00 +0530

    //

--- app/Http/Controllers/ApiManagement/DeliveryBoyController.php
+++ app/Http/Controllers/ApiManagement/DeliveryBoyController.php
@@ -959,8 +959,8 @@ private function calculateDistance($lat1, $lon1, $lat2, $lon2)
         $user = DeliveryBoy::find(Auth::user()->id);
 
         $user->update([
-            'latitude'  => $request->latitude,
-            'longitude' => $request->longitude,
+            'latitude'  => round($request->latitude, 6),
+            'longitude' => round($request->longitude, 6), 
             'address'   => $request->address ?? ''
         ]);
 
