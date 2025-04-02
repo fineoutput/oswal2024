70a2d0bd96bdf7fedbe416baa2e9c9ef8d3fe1b9
Nitesh Shah <niteshshah000@gmail.com>
2025-04-02 12:39:39 +0530

    //

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -159,7 +159,7 @@ $orders = $orders->with('orderDetails', 'user', 'address.citys', 'address.states
         $order = Order::find($decodedId);
 
         $users = User::find($order->user_id);
-        return $users;
+        // return $users;
 
     
         if (!$order) {
@@ -175,7 +175,7 @@ $orders = $orders->with('orderDetails', 'user', 'address.citys', 'address.states
             $order->rejected_by_id = $addedBy;
             $order->remarks = $validated['remarks'];
 
-            return $users->fcm_token;
+            // return $users->fcm_token;
             // $order->save();
 
             $this->sendPushNotificationVendor($users->fcm_token, $order->order_status);
