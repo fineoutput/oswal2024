aba1570ab7cef2df92fbc2cd3f7488dfbeb3041e
Ayush <ayushrs7023@gmail.com>
2025-04-02 12:50:17 +0530

    video

--- resources/views/partials/homeparts/about.blade.php
+++ resources/views/partials/homeparts/about.blade.php
@@ -280,6 +280,11 @@
     
 </div>
 
+<div class="video_about_sect">
+    <div class="container-fluid naminfs">
+        <iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/qAxrgJ0GDwc?si=BwMlxgEZydUkx5Y4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+</div>
 <!-- <div class="video_about_sect" >
 <div class="container-fluid naminfs">
 <iframe width="100%" height="100%" src="https://www.youtube.com/embed/n1TFv3Gxi4E?si=pUaKpl4Jyr44s_A8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
