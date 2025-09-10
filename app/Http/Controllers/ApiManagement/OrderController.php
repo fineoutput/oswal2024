3639698a448fe9f770541cd57d473a92ca0e8b16
Nitesh Shah <niteshshah000@gmail.com>
2025-09-10 11:48:28 +0530

    //

--- app/Http/Controllers/ApiManagement/OrderController.php
+++ app/Http/Controllers/ApiManagement/OrderController.php
@@ -1162,10 +1162,8 @@ class OrderController extends Controller
     $cur_date = now();
     $addedby = Auth::id();
 
-    // $order = Order::with('address', 'user')->find($order_id);
+    $order = Order::with('address', 'user')->find($order_id);
     
-    $order = Order::with('address', 'user.vendor')->find($order_id);
-Log::info('User with vendor check:', $order->user->toArray());
     if (!$order) {
         Session::flash('emessage', 'Order not found');
         return redirect()->back();
